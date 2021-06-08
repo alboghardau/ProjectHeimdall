3eae67119f917177a38705f43bd03e89a4ea5dbe
DESKTOP-CDF4VM5\albog <alboghardau@gmail.com>
2021-06-08 20:52:25 +0300

    Project backend init

---
+++ Backend/src/main/java/com/heimdall/ProjectHeimdallBackend/ProjectHeimdallApplication.java
@@ -0,0 +1,13 @@
+package com.heimdall.ProjectHeimdallBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHeimdallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHeimdallApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/heimdall/ProjectHeimdallBackend/ProjectHeimdallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heimdall.ProjectHeimdallBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectHeimdallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
