6169b1fbc4df79e979512b6ed65cd91e33898846
DESKTOP-CDF4VM5\albog <alboghardau@gmail.com>
2021-06-11 17:00:17 +0300

    update start data sql

--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -5,3 +5,8 @@ INSERT INTO residence(zip_code,number,latitude,longitude,residents_no) values("4
 INSERT INTO residence(zip_code,number,latitude,longitude,residents_no) values("4634434","07210134356",44.4254212,26.1032704,5);
 INSERT INTO residence(zip_code,number,latitude,longitude,residents_no) values("5653434","07238251651",44.4284212,26.1042704,2);
 INSERT INTO residence(zip_code,number,latitude,longitude,residents_no) values("6663434","07210134156",44.4214212,26.1062704,1);
+INSERT INTO residence(zip_code,number,latitude,longitude,residents_no) values("1516156","0721023456",44.4334512,26.1769704,3);
+INSERT INTO residence(zip_code,number,latitude,longitude,residents_no) values("4634342","07210135356",44.4224212,26.1862704,2);
+INSERT INTO residence(zip_code,number,latitude,longitude,residents_no) values("4634434","07210134356",44.4314212,26.1232704,5);
+INSERT INTO residence(zip_code,number,latitude,longitude,residents_no) values("5653434","07238251651",44.4278512,26.0942704,4);
+INSERT INTO residence(zip_code,number,latitude,longitude,residents_no) values("6663434","07210134156",44.4300212,26.0762704,1);
